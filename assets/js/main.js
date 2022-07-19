c71327b7638375255a583357217c9e0cdc9dab86
MD <dhmedy@googlemail.com>
2022-07-19 14:00:35 +0200

    second commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,11 @@ function fnCalculateAge() {
     let differenceBetweenNowAndBirthday = (Date.now() - date_birthday.getTime());
     let age = new Date(differenceBetweenNowAndBirthday);
 
-    // ???
+    // ??? https://www.w3resource.com/javascript-exercises/javascript-date-exercise-18.php
     let ageOutput = Math.abs(age.getUTCFullYear() - 1970);
     console.log(ageOutput);
 
-
-
     document.getElementById('divAge').innerHTML = 'Your are ' + ageOutput + ' years old!';
-
 
 }
 
